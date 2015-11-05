79cf39b731e3dbfd95893efac5ea7c25fbac403e
Jim Qin <jq@onbeep.com>
2015-11-04 21:34:36 -0800

    Initial Commit

---
+++ JQImageViewControllerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JQImageViewControllerDemo
+//
+//  Created by JQ@Onbeep on 11/4/15.
+//  Copyright © 2015 Jim Qin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
